66f99a4d34cde876c6f3d7d04da0527d7b5b9bb2
anandsyadav <anandyad2004@gmail.com>
2024-08-01 17:27:47 +0530

    update header component with user data

--- client/src/assets/components/Header.jsx
+++ client/src/assets/components/Header.jsx
@@ -1,10 +1,12 @@
-import { Button, Navbar, TextInput } from 'flowbite-react';
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
+import { useSelector } from 'react-redux';
 
 function Header() {
   const path = useLocation().pathname;
+  const { currentUser } = useSelector((state) => state.user);
 
   return (
     <Navbar className='border-b-2'>
@@ -27,9 +29,34 @@ function Header() {
           <FaMoon />
         </Button>
         <Link to='/signup'>
-          <Button gradientDuoTone='purpleToBlue' outline>
-            Sign Up
-          </Button>
+          {
+            currentUser ?
+              (
+                <Dropdown arrowIcon={false} inline label={
+                  <Avatar
+                    alt='user'
+                    img={currentUser.profilePicture}
+                    rounded
+                  />
+                }>
+                  <Dropdown.Header>
+                    <span className='block text-sm'>@{currentUser.username}</span>
+                    <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+                  </Dropdown.Header>
+                  <Link to={'/dashboard?tab=profile'}>
+                    <Dropdown.Item>Profile</Dropdown.Item>
+                  </Link>
+                  <Dropdown.Divider />
+                  <Dropdown.Item>Log Out</Dropdown.Item>
+                </Dropdown>
+              )
+              :
+              (
+                <Button gradientDuoTone='purpleToBlue' outline>
+                  Sign Up
+                </Button>
+              )
+          }
         </Link>
         <Navbar.Toggle />
       </div>
